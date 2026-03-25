3f00c93b1e1b0c41a91236b85735f5e373f90675
cuji <alexmetal66@gmail.com>
2026-03-25 01:18:33 -0500

    feat: define placeholder for web frontend

---
+++ web/app/eslint.config.mjs
@@ -0,0 +1,63 @@
+import prettier from 'eslint-config-prettier'
+import solid from 'eslint-plugin-solid'
+import globals from 'globals'
+
+import js from '@eslint/js'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        files: ['**/*.{js,jsx}']
+    },
+
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        }
+    },
+
+    js.configs.recommended,
+
+    {
+        plugins: {
+            solid
+        },
+        rules: {
+            // Solid
+            'solid/reactivity': 'warn',
+            'solid/no-destructure': 'off',
+
+            // Base rules
+            'no-console': 'warn',
+            'no-unused-vars': [
+                'warn',
+                {
+                    varsIgnorePattern: '^_',
+                    argsIgnorePattern: '^_'
+                }
+            ],
+
+            // Style (delegado parcialmente a Prettier)
+            indent: [
+                'off',
+                4,
+                {
+                    SwitchCase: 1,
+                    flatTernaryExpressions: true
+                }
+            ],
+            semi: ['error', 'never'],
+            'lines-between-class-members': ['warn', 'always', { exceptAfterSingleLine: true }]
+        }
+    },
+
+    // evita conflictos con prettier
+    prettier
+]
