0f020c8b13d00598cdae81be374d5aaeda40d2af
cuji <alexmetal66@gmail.com>
2026-03-30 22:01:08 -0500

    feat: first setup

---
+++ web/app/src/app/router.jsx
@@ -0,0 +1,10 @@
+
+export function Router() {
+    return (
+        <section>
+            <div class="">
+                <h1>Hola mundo</h1>
+            </div>
+        </section>
+    )
+}
--- web/app/src/index.jsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-
-import App from './App.jsx'
-import './index.css'
-
-render(App, document.getElementById('root'))
