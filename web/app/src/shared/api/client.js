0eaf75cdf857256061c5b8ab4e2ac227a57766c2
perdomo <dropdibemol@gmail.com>
2026-04-02 20:23:04 -0500

    task: reefine shared/api and shared/errors

--- web/app/src/shared/api/client.js
+++ web/app/src/shared/api/client.js
@@ -1,9 +1,20 @@
-import { ApiError } from '../errors'
+import {
+    ApiAbortedError,
+    ApiBadRequestError,
+    ApiConflictError,
+    ApiForbiddenError,
+    ApiInternalServerError,
+    ApiNotFoundError,
+    ApiNotImplementedError,
+    ApiRateLimitError,
+    ApiUnauthenticatedError,
+    ApiUnknownError
+} from '../errors'
 
 /**
  * @param {string} path http to request
  * @param {RequestInit} opts
- * @returns {any} api response
+ * @returns {Promise<any>} api response
  */
 export async function reqJSON(path, opts = {}) {
     const { headers, ...init } = opts
@@ -19,9 +30,32 @@ export async function reqJSON(path, opts = {}) {
     if (!r.ok) {
         const msg = await r
             .json()
-            .then(res => res.message)
+            .then(res => res?.detail?.reason || res.message)
             .catch(() => 'Can not process server response.')
-        throw new ApiError(msg)
+
+        switch (r.status) {
+            case 400:
+                throw new ApiBadRequestError(msg)
+            case 401:
+                throw new ApiUnauthenticatedError(msg)
+            case 403:
+                throw new ApiForbiddenError(msg)
+            case 404:
+                throw new ApiNotFoundError(msg)
+            case 409:
+                throw new ApiConflictError(msg)
+            case 429:
+                throw new ApiRateLimitError(msg)
+            case 499:
+                throw new ApiAbortedError(msg)
+            case 500:
+                throw new ApiInternalServerError(msg)
+            case 501:
+                throw new ApiNotImplementedError(msg)
+
+            default:
+                throw new ApiUnknownError(msg)
+        }
     }
 
     return await r.json()
--- web/app/src/shared/errors.js
+++ web/app/src/shared/errors.js
@@ -1,3 +1,43 @@
-export class ApiError extends Error {}
+// ─────────────────────────────────────────────────────────────────────────────
+// API ERRORS (ApiError)
+// ─────────────────────────────────────────────────────────────────────────────
+export class ApiError extends Error {
+    /** @param {string} message */
+    constructor(message) {
+        super(message)
+    }
+}
 
-export class ValidationError extends Error {}
+export class ApiInternalServerError extends ApiError {}
+
+export class ApiUnauthenticatedError extends ApiError {}
+
+export class ApiForbiddenError extends ApiError {}
+
+export class ApiNotFoundError extends ApiError {}
+
+export class ApiConflictError extends ApiError {}
+
+export class ApiRateLimitError extends ApiError {}
+
+export class ApiAbortedError extends ApiError {}
+
+export class ApiNotImplementedError extends ApiError {}
+
+export class ApiBadRequestError extends ApiError {}
+
+export class ApiUnknownError extends ApiError {}
+
+// ─────────────────────────────────────────────────────────────────────────────
+// FRONTEND ERRORS (ClientError)
+// ─────────────────────────────────────────────────────────────────────────────
+export class ClientError extends Error {}
+
+export class ValidationError extends ClientError {
+    /** @param {string} message */
+    /** @param {import('zod').ZodError | undefined} issues */
+    constructor(message, issues) {
+        super(message)
+        this.issues = issues
+    }
+}
