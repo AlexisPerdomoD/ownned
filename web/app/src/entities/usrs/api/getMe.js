ceec950e4821a0433263246bfe05acd8ca70e1ae
perdomo <dropdibemol@gmail.com>
2026-04-11 23:10:33 -0500

    fix: properly add eslint support

--- web/app/src/entities/usrs/api/getMe.js
+++ web/app/src/entities/usrs/api/getMe.js
@@ -5,7 +5,7 @@ import { reqJSON } from '@shared/api/client'
  * Get the current user if logged in. Else, return null.
  *
  * @throws {import('@shared/errors').ApiError} if api returns an error diferent from 401.
- * @returns {Promise<import('@/entities').Usr | null }
+ * @returns {Promise<import('@/entities/usrs').Usr | null }
  */
 export async function apiGetMe() {
     return await reqJSON('/api/v1/usrs/me').catch(err => {
